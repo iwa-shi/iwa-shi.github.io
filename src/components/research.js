11fc6d63d6d71f21ca89d4e1d2b2d2cf3be56338
sho-iwai <sho.iwai101@gmail.com>
2024-05-27 18:10:19 +0900

    update profile

--- src/components/research.js
+++ src/components/research.js
@@ -30,10 +30,10 @@ const ResearchArea = () => {
             <SectionTitle title="Study and Research" />
             <Box sx={{m: 1}}>
             <Typography variant='body1'>
-            I'm a second year PhD student at Tohoku University. I'm studying <span className='green-font bold'>Learned Image Compression</span> at <Link href="http://www.iic.ecei.tohoku.ac.jp/" target="_blank" color="inherit">Image Information Communication Lab</Link>, where I am supervised by <Link href="http://www.iic.ecei.tohoku.ac.jp/~machi/index-e.html" target="_blank" color="inherit">Prof. Shinichiro Omachi</Link>.
+            He is a third-year PhD student at Tohoku University, specializing in <span className='green-font bold'>Learned Image Compression</span> at <Link href="http://www.iic.ecei.tohoku.ac.jp/" target="_blank" color="inherit">Image Information Communication Lab</Link>, under the supervision of <Link href="http://www.iic.ecei.tohoku.ac.jp/~machi/index-e.html" target="_blank" color="inherit">Prof. Shinichiro Omachi</Link>.
             </Typography>
             <Typography variant='body1'>
-            I'm also a member of <Link href='https://www.aie.tohoku.ac.jp/english/greeting/' color="inherit">AIE WISE Program for AI Electronics</Link> and <Link href='https://pgd.tohoku.ac.jp/english/rpc/next_generation.html' color="inherit">Pioneering Research Support Project for PhD Students</Link>.
+            He is a recipient of the <span className='green-font bold'>JSPS Research Fellowship for Young Scientists (DC2)</span> and is also a member of <Link href='https://www.aie.tohoku.ac.jp/english/greeting/' color="inherit">AIE WISE Program for AI Electronics</Link>.
             </Typography>
             </Box>
             <Box sx={{m: 1}}>
